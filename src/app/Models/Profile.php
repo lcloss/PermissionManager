7a38b1bd743e60605db1910e35136ee1a8ac420f
Luciano Closs <lcloss@gmail.com>
2020-05-25 00:04:16 +0100

    Add CrudTrait to Profiles

--- src/app/Models/Profile.php
+++ src/app/Models/Profile.php
@@ -2,10 +2,13 @@
 
 namespace Backpack\PermissionManager\app\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    use CrudTrait;
+    
     protected $fillable = ['user_id', 'avatar', 'birthdate', 'resume', 'facebook_url', 'linkedin_url', 'twitter_url', 'instagram_url', 'youtube_url'];
 
     public function getAvatarAttribute($avatar) 
